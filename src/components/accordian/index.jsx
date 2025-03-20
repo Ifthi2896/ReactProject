175484c84d83d05c2ddcf167b01c8ff7b9a69942
IFTHI <mifthikarudeen@gmail.com>
2025-03-20 16:12:09 +0530

    accordion Project is created

---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import data from "./data";
+import "./style.css";
+
+export default function Accordion() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSingleSelection(selectedId) {
+    setSelected(selectedId === selected ? null : selectedId);
+  }
+
+  function handleMultiSelection(selectedId) {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(selectedId);
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(selectedId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+    setMultiple(cpyMultiple);
+  }
+  console.log(multiple, selected);
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div
+              key={dataItem.id}
+              className="item"
+              onClick={
+                enableMultiSelection
+                  ? () => handleMultiSelection(dataItem.id)
+                  : () => handleSingleSelection(dataItem.id)
+              }
+            >
+              <div className="title">
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
+            </div>
+          ))
+        ) : (
+          <div>no data found !</div>
+        )}
+      </div>
+    </div>
+  );
+}
